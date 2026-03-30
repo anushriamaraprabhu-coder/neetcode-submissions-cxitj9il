554938066e0a1346e3263d714c487c7e2bab9d76
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-03-30 10:00:33 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,43 @@
+class Solution {
+    public static boolean isAnagram(String s, String t) {
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        String s1=s.toLowerCase();
+        String t1=t.toLowerCase();
+
+        HashMap<Character,Integer>map=new HashMap<>();
+
+        //increasing count of first string
+
+        for(char ch:s1.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        //second loop for reducing the count to balance out the first string
+
+        for(char ch:t1.toCharArray()){
+            if(!map.containsKey(ch)|| map.get(ch)==0){
+            return false;
+            }
+             map.put(ch,map.get(ch)-1);
+        }
+        return true;
+        
+        
+
+    }
+
+    public static void main(String[]args){
+
+        String s="racecar";
+        String t="carrace";
+
+        isAnagram(s,t);
+
+
+        
+    }
+}
