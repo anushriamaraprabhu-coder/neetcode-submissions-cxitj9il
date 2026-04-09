40f0d7219a89c22036a7628ce2fce2fe95a46dfa
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-04-09 09:05:33 +0530

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int search(int[] nums, int target) {
+
+        for(int i=0;i<nums.length;i++){
+            if(target==nums[i]){return i;}
+        }
+        return -1;
+    }
+     public static void main(String[]args){
+
+        int[]nums={-1,0,2,4,6,8};
+        int target=4;
+        search(nums,target);
+
+            
+        }
+}
