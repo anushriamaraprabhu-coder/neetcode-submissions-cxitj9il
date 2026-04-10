3abf118b541c237b36f522d5e0e8b75b072c1459
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-04-10 12:12:11 +0530

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        if(n==1){return 1;}
+
+        int[]ways_to_climb_stairs=new int[n+1];
+        ways_to_climb_stairs[1]=1;
+        ways_to_climb_stairs[2]=2;
+
+        
+
+        for(int i=3;i<=n;i++){
+            ways_to_climb_stairs[i]=ways_to_climb_stairs[i-1]+ways_to_climb_stairs[i-2];
+        }
+        return ways_to_climb_stairs[n];
+        
+    }
+}
