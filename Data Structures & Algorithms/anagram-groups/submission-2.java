a18deec8bd0d453ce6a0fa202b86cc2b66eaef2b
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-04-13 10:42:50 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,49 @@
+class Solution {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        if(strs==null || strs.length==0){return new ArrayList<>();}
+
+        HashMap<String,List<String>>map=new HashMap<>();
+
+        for(String str:strs){
+
+            String freqString=getFrequencyOfString(str);
+
+            if(map.containsKey(freqString)){
+                map.get(freqString).add(str);
+            }
+
+            else{
+                List<String>list=new ArrayList<>();
+                list.add(str);
+                map.put(freqString, list);
+            }
+        }
+
+        return new ArrayList<>(map.values());
+
+    }
+
+    private String getFrequencyOfString(String str){
+
+        int[]freq=new int[26];
+        for(char ch:str.toCharArray()){
+            freq[ch-'a']++;
+
+        }
+
+        StringBuilder sb=new StringBuilder("ch");
+        char ch='a';
+        for(int i:freq){
+            sb.append(ch);
+            sb.append(i);
+
+            ch++;
+
+        }
+
+        return sb.toString();
+
+    }
+}
