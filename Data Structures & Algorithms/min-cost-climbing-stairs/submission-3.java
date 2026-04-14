0b9998e9358f6359c18a7a5235901d1746434bb4
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-04-14 16:04:24 +0530

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+
+        int len=cost.length;
+
+        int[]minCost= new int[len+1];
+
+        for(int i=2;i<=len;i++){
+            minCost[i]=Math.min(
+                (cost[i-1]+minCost[i-1]), (cost[i-2]+minCost[i-2])
+            );
+        }
+
+        return minCost[len];
+        
+    }
+}
