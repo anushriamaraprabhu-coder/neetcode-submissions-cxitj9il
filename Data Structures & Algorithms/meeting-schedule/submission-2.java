9291cdca2defc388db69c5b71d1a06ef05d2fa37
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-04-14 16:09:56 +0530

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.java
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+
+        Collections.sort(intervals,(a,b)->a.start-b.start);
+
+        for(int i=1;i<intervals.size();i++){
+            Interval prev=intervals.get(i-1);
+            Interval current=intervals.get(i);
+
+            if(current.start<prev.end){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
