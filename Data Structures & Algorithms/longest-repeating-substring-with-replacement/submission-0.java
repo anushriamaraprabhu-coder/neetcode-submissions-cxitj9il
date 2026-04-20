3f5550e3f4f040d8b5c6f5cc32b0a4c0d3176089
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-04-20 13:50:57 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        int[]freq=new int[26];
+
+        int left=0;
+        int maxWindow=0;
+        int maxFreq=0;
+
+        //start the loop and right goes all the way till the end 
+
+        for(int right=0;right<s.length();right++){
+
+            freq[s.charAt(right)-'A']++;
+
+            //update the maxFrequency 
+
+            maxFreq=Math.max(maxFreq,freq[s.charAt(right)-'A']);
+
+            int windowLength=right-left+1;
+
+            if(windowLength-maxFreq >k){
+                freq[s.charAt(left)-'A']--;
+                left++;
+
+            }
+
+            windowLength=right-left+1;
+            maxWindow=Math.max(windowLength,maxWindow);
+
+
+
+            
+        }
+
+        return maxWindow;
+        
+    }
+}
