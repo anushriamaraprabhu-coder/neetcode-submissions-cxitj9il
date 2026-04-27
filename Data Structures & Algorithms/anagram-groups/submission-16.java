aa292358436c7028f74eb0695997182922628243
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-04-27 19:10:10 +0530

    Add: anagram-groups - submission-16

---
+++ Data Structures & Algorithms/anagram-groups/submission-16.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String,List<String>>map=new HashMap<>();
+        if(strs==null||strs.length==0){return new ArrayList<>(); }
+
+        for(String str:strs){
+            String freqString=getFrequencyOfStrings(str);
+            if(map.containsKey(freqString)){
+                map.get(freqString).add(str);
+            }
+
+            else{
+
+                List<String>list=new ArrayList<>();
+                list.add(str);
+                map.put(freqString, list);
+            }
+        }
+
+        return new ArrayList<>(map.values());
+        
+    }
+
+    private String getFrequencyOfStrings(String str){
+
+        int[]freq=new int[26];
+        for(char ch:str.toCharArray()){
+            freq[ch-'a']++;
+        }
+
+        StringBuilder sb=new StringBuilder("");
+        char ch='a';
+        for(int i:freq){
+            sb.append(ch);
+            sb.append(i);
+        }
+        return sb.toString();
+    }
+}
