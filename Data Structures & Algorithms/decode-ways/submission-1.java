7af4a857c066212c75bd1a1dde95f75516e63348
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-05-13 21:15:23 +0530

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numDecodings(String s) {
+
+        if(s.charAt(0)=='0'){return 0;}
+
+        int prev2=1;//d[0]
+        int prev1=1;//d[1]
+
+        for(int i=2;i<=s.length();i++){
+
+            int current=0;
+
+            int onedigit=Integer.valueOf(s.substring(i-1,i));
+            int twodigit=Integer.valueOf(s.substring(i-2,i));
+
+            if(onedigit>=1){
+                current+=prev1;
+            }
+            if(twodigit>=10 && twodigit<=26){
+                current+=prev2;
+            }
+
+            prev2=prev1;
+            prev1=current;
+        }
+
+        return prev1;
+        
+    }
+}
