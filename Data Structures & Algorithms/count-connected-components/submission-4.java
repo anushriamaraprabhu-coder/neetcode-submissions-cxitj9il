2e8622e14587327a09b7af9034f25c64bdaf68fe
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-05-13 22:54:20 +0530

    Add: count-connected-components - submission-4

---
+++ Data Structures & Algorithms/count-connected-components/submission-4.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+
+        //timecomplexity: O(V+E)
+        //space complexity: O(V+E)
+
+        List<List<Integer>>adjacencylist=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adjacencylist.add(new ArrayList<>());
+        }
+
+        for(int[]edge:edges){
+
+            adjacencylist.get(edge[0]).add(edge[1]);
+            adjacencylist.get(edge[1]).add(edge[0]);
+
+        }
+
+        HashSet<Integer>visited=new HashSet<>();
+        Stack<Integer>stack=new Stack<>();
+
+         int components=0;
+
+        for(int i=0;i<n;i++){
+
+           
+
+            if(!visited.contains(i)){
+                components++;
+            }
+
+            stack.push(i);
+            visited.add(i);
+
+            while(!stack.isEmpty()){
+                int currentvertex=stack.pop();
+
+                for(int neighbours:adjacencylist.get(currentvertex)){
+
+                    if(!visited.contains(neighbours)){
+                        stack.push(neighbours);
+                        visited.add(neighbours);
+                    }
+                }
+            }
+        }
+
+        return components;
+
+    }
+}
