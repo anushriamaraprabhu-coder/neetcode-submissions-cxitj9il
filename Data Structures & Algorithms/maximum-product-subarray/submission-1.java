43640ecf066bda9f968908504ca987f2b3a62e61
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-05-14 17:56:03 +0530

    Add: maximum-product-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+
+        //timecomplexity= O(n);
+        //space complexity: O(1);
+
+        int len=nums.length;
+        int leftproduct=1;
+        int rightproduct=1;
+        int result=nums[0];
+
+
+        for(int i=0;i<len;i++){
+
+            leftproduct=leftproduct==0? 1:leftproduct;
+            rightproduct=rightproduct==0?1:rightproduct;
+
+            //prefix product
+            leftproduct*=nums[i];
+
+            //suffix product
+            rightproduct*=nums[len-1-i];
+
+
+            result=Math.max(result,Math.max(leftproduct,rightproduct));
+        }
+
+        return result;
+        
+    }
+}
