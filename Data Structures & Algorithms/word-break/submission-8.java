b45351785f41903da147a08591a6631251b42ed0
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-05-24 12:05:32 +0530

    Add: word-break - submission-8

---
+++ Data Structures & Algorithms/word-break/submission-8.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+        int maxlen=0;
+
+        HashSet<String>set=new HashSet<>(wordDict);
+
+        for(String word:wordDict){
+             maxlen=Math.max(maxlen,word.length());
+        }
+
+
+        int len=s.length();
+
+        boolean[]dp=new boolean[len+1];
+
+        dp[0]=true;
+
+        for(int i=1;i<=len;i++){
+            for(int j=i-1;j>=Math.max(0,i-maxlen);j--){
+                if(dp[j] && set.contains(s.substring(j,i))){
+                    dp[i]= true;
+                    break;
+                }
+            }
+        }
+
+        return dp[len];
+        
+    }
+}
