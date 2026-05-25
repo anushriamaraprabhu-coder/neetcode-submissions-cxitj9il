137d31488b61107c52eaa1b42298748dbe5498b8
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-05-25 07:57:30 +0530

    Add: house-robber - submission-14

---
+++ Data Structures & Algorithms/house-robber/submission-14.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        if(nums.length<2){return nums[0];}
+
+        int prev1=Math.max(nums[0],nums[1]);
+        int prev2=nums[0];
+
+        for(int i=2;i<nums.length;i++){
+
+            int current= Math.max(prev2+nums[i],prev1);
+
+            prev2=prev1;
+            prev1=current;
+        }
+        
+        return prev1;
+    }
+}
