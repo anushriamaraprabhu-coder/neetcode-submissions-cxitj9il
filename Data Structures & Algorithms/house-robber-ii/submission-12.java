baeff1d3a0dbd9f725d083515b7f3a0a66c87dc0
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-05-25 08:04:47 +0530

    Add: house-robber-ii - submission-12

---
+++ Data Structures & Algorithms/house-robber-ii/submission-12.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        if(nums.length<2){return nums[0];}
+
+        int skiplasthouse=robbing(nums,0,nums.length-2);
+        int skipfirsthouse=robbing(nums,1,nums.length-1);
+
+
+        return Math.max(skiplasthouse,skipfirsthouse);
+        
+    }
+
+    private int robbing(int[]nums,int start,int end){
+
+        int prev1=0;
+        int prev2=0;
+
+        for(int i=start;i<=end;i++){
+
+            int current=Math.max(prev2+nums[i],prev1);
+
+            prev2=prev1;
+            prev1=current;
+        }
+
+        return prev1;
+    }
+}
