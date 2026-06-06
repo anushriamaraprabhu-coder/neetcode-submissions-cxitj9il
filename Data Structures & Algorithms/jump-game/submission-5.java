785551d2141ca5e5e14469d89ab5b1b4fd42039a
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-06-06 11:06:22 +0530

    Add: jump-game - submission-5

---
+++ Data Structures & Algorithms/jump-game/submission-5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+
+        int finalindex=nums.length-1;
+
+        for(int index=nums.length-2;index>=0;index--){
+            if(index+nums[index]>=finalindex)
+            {
+                finalindex=index;
+                }
+        }
+
+        return finalindex==0;
+        
+    }
+}
