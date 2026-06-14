21facd505b427226d3a3a12395bfb98817d4a268
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-06-14 08:16:03 +0530

    Add: minimum-window-with-characters - submission-4

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-4.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String minWindow(String s, String t) {
+
+        int[]freq=new int[128];
+        for(char ch:t.toCharArray()){
+            freq[ch]++;
+        }
+
+        int left=0;
+        int minlen=Integer.MAX_VALUE;
+        int start=0;
+        int count=t.length();
+
+        for(int right=0;right<s.length();right++){
+
+            char charAtright=s.charAt(right);
+
+            if(freq[charAtright]>0){
+                count--;
+            }
+            
+            freq[charAtright]--;
+
+            while(count==0){
+
+                char charAtleft=s.charAt(left);
+
+                if(right-left+1<minlen){
+                    minlen=right-left+1;
+                    start=left;
+                }
+
+                freq[charAtleft]++;
+
+                if(freq[charAtleft]>0){
+                    count++;
+                }
+
+
+                left++;
+                
+            }
+
+        }
+
+        return minlen==Integer.MAX_VALUE ? "" : s.substring(start,start+minlen);
+
+
+        
+    }
+}
