2f3060f83b844b59de5003d642a73d6197b5afb9
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-09-06 18:18:08 +0530

    Add: buy-and-sell-crypto - submission-10

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-10.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int buyprice=prices[0];
+        int maxprofit=0;
+        
+        for(int price:prices){
+
+            int currentprofit=price-buyprice;
+
+            maxprofit=Math.max(currentprofit,maxprofit);
+
+            if(price<buyprice){
+                buyprice=price;
+            }
+
+            
+        }
+
+        return maxprofit;
+        
+    }
+}
