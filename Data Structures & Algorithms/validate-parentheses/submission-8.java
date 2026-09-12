54a9a5f1f81739f92ec460930ad97d90edc18ff6
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-09-12 11:15:52 +0530

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        Deque<Character>stack=new ArrayDeque<>();
+
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{'){
+                stack.push(ch);
+            }
+            else{
+
+                if(stack.isEmpty()){
+                    return false;
+                }
+                
+                if(ch==')' && stack.peek()=='('
+                   || ch=='}' && stack.peek()=='{'
+                   || ch==']' && stack.peek()=='['){
+
+                    stack.pop();
+                   }
+                
+                else{
+
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+        
+    }
+}
