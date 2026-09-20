9601d814d3d0308ed247701425a2eb2fa1a09bfa
anushriamaraprabhu-coder <anushri.amaraprabhu@gmail.com>
2026-09-20 09:11:51 +0530

    Add: is-anagram - submission-19

---
+++ Data Structures & Algorithms/is-anagram/submission-19.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        HashMap<Character,Integer>map=new HashMap<>();
+        
+
+        for(char ch:s.toCharArray()){
+
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        for(char ch:t.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)-1);
+        }
+
+        for(int count:map.values()){
+            if(count!=0){
+                return false;
+            }
+            // else{
+            //     return true;
+            // }
+        }
+        return true;
+
+    }
+}
